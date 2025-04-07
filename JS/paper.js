c8d12b86517b691f9df05e8e787584d9d66a29ac
NickusTop <148118087+NickusTop@users.noreply.github.com>
2025-04-07 19:15:00 +0300

    ed

--- JS/paper.js
+++ JS/paper.js
@@ -2,13 +2,15 @@ const stone = document.getElementById("stone");
 const scissors = document.getElementById("scissors");
 const paper = document.getElementById("paper");
 const stscResult = document.getElementById("stscResult");
-const stscButton = document.getElementById("stscButton");
+const stscP = document.getElementById("stscP");
 const aiScore = document.getElementById("ai-score");
 const plScore = document.getElementById("pl-score");
 
 let plChoice = 0;
-let aiScor = 0;
-let plScor = 0;
+let aiScoreCount = 0;
+let plScoreCount = 0;
+let varia = "";
+let varia2 = "";
 
 stone.addEventListener("click", () => setChoice(1));
 scissors.addEventListener("click", () => setChoice(2));
@@ -16,9 +18,8 @@ paper.addEventListener("click", () => setChoice(3));
 
 function setChoice(choice) {
   plChoice = choice;
+  stscGame();
 }
-
-stscButton.addEventListener("click", stscGame);
 
 function stscGame() {
   if (plChoice === 0) {
@@ -27,7 +28,24 @@ function stscGame() {
   }
 
   let aiChoice = Math.floor(Math.random() * 3) + 1;
-  console.log(`Гравець: ${plChoice}, Комп’ютер: ${aiChoice}`);
+
+  if (plChoice === 1) {
+    varia = "Камінь";
+  } else if (plChoice === 2) {
+    varia = "Ножниці";
+  } else if (plChoice === 3) {
+    varia = "Папір";
+  }
+
+  if (aiChoice === 1) {
+    varia2 = "Камінь";
+  } else if (aiChoice === 2) {
+    varia2 = "Ножниці";
+  } else if (aiChoice === 3) {
+    varia2 = "Папір";
+  }
+
+  stscP.textContent = `Гравець: ${varia}, Комп’ютер: ${varia2}`;
 
   if (
     (plChoice === 1 && aiChoice === 2) ||
@@ -35,12 +53,14 @@ function stscGame() {
     (plChoice === 3 && aiChoice === 1)
   ) {
     stscResult.textContent = "Ви виграли раунд!";
-    plScor++;
-    plScore.textContent = `Ви - ${plScor}`;
+    plScoreCount++;
+    plScore.textContent = `Ви - ${plScoreCount}`;
+  } else if (plChoice === aiChoice) {
+    stscResult.textContent = "Нічия!";
   } else {
     stscResult.textContent = "Комп’ютер виграв раунд!";
-    aiScor++;
-    aiScore.textContent = `Комп’ютер - ${aiScor}`;
+    aiScoreCount++;
+    aiScore.textContent = `Комп’ютер - ${aiScoreCount}`;
   }
 
   plChoice = 0;
